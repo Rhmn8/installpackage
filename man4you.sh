16d366891da665e1120651e78a3521dffa39feef
Aulia Rahman gnteng <83273573+Rhmn8@users.noreply.github.com>
2021-12-15 12:42:42 +0700

    Add files via upload

---
+++ man4you.sh
@@ -0,0 +1,45 @@
+## install package yg belom diinstall!
+
+## author by rahman ganz
+
+## thanks too : allah swt, ortua, aulia rahman, mastah dll!
+
+## thanks too nya jangan diubah!
+
+
+
+pkg install update
+pkg install upgrade
+pkg install git
+pkg install python2
+pkg install nodejs
+pkg install ffmpeg
+pkg install figlet
+pip2 install lolcat
+pip2 install bs4
+pip2 install requests
+pip2 install mechanize
+
+
+
+clear
+
+echo "sukses install package"
+
+sleep 5
+
+clear
+
+echo "silahkan ngehack sepuas puas kalian"
+
+sleep 5
+
+clear
+
+echo "gak bisa ubah sdcard? oh oke tapi tunggu 7 detik!"
+
+clear
+
+termux-setup-storage
+
+echo "selamat sdcard nya bisa ubah di home berkas hp mu"
